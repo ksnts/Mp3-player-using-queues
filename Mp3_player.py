9466395d57c5728a79477cbd62efa79a66625d94
John Kylle Santos <santosjohnkyllem@gmail.com>
2023-02-16 23:49:32 +0800

    added select song function for adding a specific song rather than the initial coded title. It then adds the song into a queue which plays after the first song finishes

--- Mp3_player.py
+++ Mp3_player.py
@@ -1,31 +1,47 @@
-
-#mp3 player initial code
 from pygame import mixer
 from tkinter import *
+from tkinter import filedialog
 
 root = Tk()
 root.geometry("600x300")
 
 mixer.init()
-mixer.music.load("crab.mp3")
 
+# Create an empty list to hold the songs in the queue
+queue = []
 
 def pause():
     mixer.music.pause()
 
-
 def play():
-    mixer.music.play()
-
+    # If the queue is not empty, play the first song in the queue
+    if queue:
+        song_path = queue.pop(0)
+        mixer.music.load(song_path)
+        mixer.music.play()
+    else:
+        mixer.music.play()
 
 def resume():
     mixer.music.unpause()
 
+def select_song():
+    song_path = filedialog.askopenfilename()
+    queue.append(song_path)
+
+def quit():
+    # Stop any music that's currently playing
+    mixer.music.stop()
+    # Close the window
+    root.destroy()
 
 Label(root, text="Welcome to music player", font="lucidia 30 bold").pack()
-Button(text="Play", command=play).place(x=200, y=100)
-Button(text="Pause", command=pause).place(x=250, y=100)
-Button(text="Resume", command=resume).place(x=310, y=100)
-Button(text="Quit", command=quit).place(x=380, y=100)
+
+Button(text="Select Song", command=select_song).place(x=150, y=100)
+Button(text="Add to Queue", command=select_song).place(x=250, y=100)
+Button(text="Play", command=play).place(x=310, y=100)
+Button(text="Pause", command=pause).place(x=380, y=100)
+Button(text="Resume", command=resume).place(x=450, y=100)
+Button(text="Quit", command=quit).place(x=520, y=100)
 
 root.mainloop()
