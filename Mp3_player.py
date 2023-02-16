ed35a8844e4298e49f0f2b7b52cb2df0b2659795
John Kylle Santos <santosjohnkyllem@gmail.com>
2023-02-17 00:16:39 +0800

    Added shuffle in the player where it shuffles the songs in queue

--- Mp3_player.py
+++ Mp3_player.py
@@ -1,6 +1,7 @@
 from pygame import mixer
 from tkinter import *
 from tkinter import filedialog
+import random
 
 root = Tk()
 root.geometry("600x300")
@@ -11,9 +12,11 @@ mixer.init()
 queue = []
 
 def pause():
+    global queue
     mixer.music.pause()
 
 def play():
+    global queue
     # If the queue is not empty, play the first song in the queue
     if queue:
         song_path = queue[0]
@@ -25,13 +28,20 @@ def play():
         mixer.music.play()
 
 def resume():
+    global queue
     mixer.music.unpause()
 
 def select_song():
+    global queue
     song_path = filedialog.askopenfilename()
     queue.append(song_path)
 
+def shuffle_queue():
+    global queue
+    random.shuffle(queue)
+
 def skip():
+    global queue
     # Stop any music that's currently playing
     mixer.music.stop()
     # If the queue is not empty, play the first song in the queue
@@ -39,6 +49,7 @@ def skip():
         play()
 
 def quit():
+    global queue
     # Stop any music that's currently playing
     mixer.music.stop()
     # Close the window
@@ -51,8 +62,8 @@ Button(text="Add to Queue", command=select_song).place(x=250, y=100)
 Button(text="Play", command=play).place(x=310, y=100)
 Button(text="Pause", command=pause).place(x=380, y=100)
 Button(text="Resume", command=resume).place(x=450, y=100)
-Button(text="Skip", command=skip).place(x=520, y=100)
+Button(text="Shuffle Queue", command=shuffle_queue).place(x=520, y=100)
+Button(text="Skip", command=skip).place(x=150, y=150)
 Button(text="Quit", command=quit).place(x=570, y=100)
 
 root.mainloop()
-
